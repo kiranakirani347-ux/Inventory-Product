1e9166d0e18f602cc8f1d2add0d10a2c91c63d88
kiranakirani347 <kiranakirani347@gmail.com>
2026-05-20 08:48:37 +0700

    edit file logout.php

--- logout.php
+++ logout.php
@@ -1,2 +1,13 @@
 <?php
-session_start
+session_start();
+
+// Hapus semua data session
+$_SESSION = [];
+
+// Hancurkan session
+session_destroy();
+
+// Redirect ke halaman login atau home
+header("Location: login.php");
+exit;
+?>
